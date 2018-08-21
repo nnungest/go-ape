21f156f0253745e67bae7e215aa9ec07d8bc8fd0
nnungest <nnungest@gmail.com>
2018-08-21 15:14:16 -0400

    added actual params for routes

--- main.go
+++ main.go
@@ -6,29 +6,34 @@ import (
 	"net/http"
 )
 
+// HWhandler for somethign
 func HWhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
-}
-func Reversehandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "dlrow olleh")
-}
-func Upperhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HELLO WORLD")
 
+	if r.URL.Query().Get("uppercase") == "true" {
+		fmt.Fprintf(w, "HELLO WORLD")
+	} else if r.URL.Query().Get("uppercase") == "false" {
+		fmt.Fprintf(w, "hello world")
+	} else if r.URL.Query().Get("reverse") == "true" {
+		fmt.Fprintf(w, "dlrow olleh")
+	} else if r.URL.Query().Get("reverse") == "false" {
+		fmt.Fprintf(w, "hello world")
+	} else {
+		fmt.Fprintf(w, "hello world")
+	}
 }
+
+// Worldhandler for world string only
 func Worldhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "world")
 }
+
+// Hellohandler for hello string only
 func Hellohandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
 }
 
 func main() {
 	http.HandleFunc("/", HWhandler)
-	http.HandleFunc("/uppercase=false", HWhandler)
-	http.HandleFunc("/uppercase=true", Upperhandler)
-	http.HandleFunc("/reverse=false", HWhandler)
-	http.HandleFunc("/reverse=true", Reversehandler)
 	http.HandleFunc("/hello", Hellohandler)
 	http.HandleFunc("/world", Worldhandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
