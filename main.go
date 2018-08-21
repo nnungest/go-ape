1be67345099ab000c8cc44dc6b5a2ffaac3ab765
nnungest <nnungest@gmail.com>
2018-08-21 01:58:38 -0400

    added extra endpoints (workaround)

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 func HWhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world")
 }
+func Reversehandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "dlrow olleh")
+}
+func Upperhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HELLO WORLD")
+
+}
 func Worldhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "world")
 }
@@ -18,6 +25,10 @@ func Hellohandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", HWhandler)
+	http.HandleFunc("/uppercase=false", HWhandler)
+	http.HandleFunc("/uppercase=true", Upperhandler)
+	http.HandleFunc("/reverse=false", HWhandler)
+	http.HandleFunc("/reverse=true", Reversehandler)
 	http.HandleFunc("/hello", Hellohandler)
 	http.HandleFunc("/world", Worldhandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
